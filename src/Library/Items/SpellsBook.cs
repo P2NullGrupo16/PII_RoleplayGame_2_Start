98906fe52c15fe43ea8a170adc45321d2874074b
jumpert <juan.perezc@correo.ucu.edu.uy>
2021-09-22 20:28:35 -0300

    Interface Valores Defensa Items

--- src/Library/Items/SpellsBook.cs
+++ src/Library/Items/SpellsBook.cs
@@ -1,6 +1,6 @@
 using System.Collections.Generic;
 
-namespace RoleplayGame
+namespace RoleplayGame : IItemDefense
 {
     public class SpellsBook
     {
--- src/Library/Items/Sword.cs
+++ src/Library/Items/Sword.cs
@@ -1,6 +1,6 @@
 namespace RoleplayGame
 {
-    public class Sword
+    public class Sword : IItemDefense
     {
         public int AttackValue 
         {
