c7767b113f79e3e78465ee8ca8207fc7ea0a7fd9
Tomás Rama <tomas.rama@correo.ucu.edu.uy>
2021-09-25 04:20:52 -0300

    Agregada interface para personaje

---
+++ src/Library/Characters/CharacterInterface.cs
@@ -0,0 +1,15 @@
+using System;
+
+namespace RoleplayGame
+{
+
+
+    public interface ICharacter
+    {
+        int AttackValue {get;}
+        int DefenseValue {get;}
+        int Health {get;}
+        void ReceiveAttack(int power);
+        void Cure();
+    }
+}
